c851430b466144dc5c1b21e60328d4439de7b6bb
William Kennedy <williamkennedyg7@gmail.com>
2025-10-02 23:06:40 -0300

    Added persister pandas.

--- run.py
+++ run.py
@@ -1,14 +1,21 @@
 import pandas as pd
 from hook import HookPandas
 from ingester import IngesterPandas
+from persister import PersisterPandas
 
 def main():
-	file_name = "datasets/Vendas.csv"
+	input_file_name = "datasets/Vendas.csv"
+	output_file_name = "datasets/Vendas_out.csv"
 	hook = HookPandas()
-	ingester = IngesterPandas(hook, file_name)
+	ingester = IngesterPandas(hook, input_file_name)
+	persister = PersisterPandas(hook, output_file_name)
 
 	df = ingester.ingest()
 	print(df, df.columns, len(df))
+
+	df = df.assign(new_column="new value")
+	print(df, df.columns, len(df))
+	persister.persist(df)
 
 if __name__ == "__main__":
 	main()
