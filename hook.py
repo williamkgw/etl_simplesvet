3fc62000cd9585fbe0661ffb7e28d1bbe71d1f78
William Kennedy <williamkennedyg7@gmail.com>
2025-10-02 21:43:56 -0300

    Added runner with working pandas ingester.

---
+++ hook.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+class HookPandasWriter:
+
+	def __init__(self):
+		self._options = dict()
+		self.csv = lambda df: df.to_csv(**self._options)
+
+	def option(self, option_key, option_value):
+		self._options.update({
+			option_key: option_value
+		})
+		return self
+
+	def options(self, options_dict):
+		self._options.update(options_dict)
+		return self
+
+class HookPandasReader:
+
+	def __init__(self):
+		self._options = dict()
+		self.csv = lambda : pd.read_csv(**self._options)
+
+	def option(self, option_key, option_value):
+		self._options.update({
+			option_key: option_value
+		})
+		return self
+
+	def options(self, options_dict):
+		self._options.update(options_dict)
+		return self
+
+class HookPandas:
+
+	def connect(self):
+		self.writer = HookPandasWriter()
+		self.reader = HookPandasReader()
+		return self
--- run.py
+++ run.py
@@ -1,18 +1,14 @@
 import pandas as pd
+from hook import HookPandas
+from ingester import IngesterPandas
 
 def main():
+	file_name = "datasets/Vendas.csv"
+	hook = HookPandas()
+	ingester = IngesterPandas(hook, file_name)
 
-	data = {
-	    "id": [1, 2, 3, 4, 5],
-	    "nome": ["Alice", "Bruno", "Carla", "Diego", "Eva"],
-	    "idade": [25, 30, 22, 35, 28],
-	    "cidade": ["São Paulo", "Rio de Janeiro", "Belo Horizonte", "Curitiba", "Recife"],
-	    "score": [85.5, 90.2, 76.8, 88.0, 92.3]
-	}
-
-	df = pd.DataFrame(data)
-
-	print(df.to_json(orient='records'))
+	df = ingester.ingest()
+	print(df, df.columns, len(df))
 
 if __name__ == "__main__":
 	main()
