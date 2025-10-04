c55895f328e0d5712bd071a9811b5bbde301ca32
William Kennedy <williamkennedyg7@gmail.com>
2025-10-04 16:11:22 -0300

    Done a simple transformer using pandas dataframe.

--- run.py
+++ run.py
@@ -1,21 +1,22 @@
-import pandas as pd
 from hook import HookPandas
 from ingester import IngesterPandas
 from persister import PersisterPandas
+from transformer import TransformerPandas
 
 def main():
-	input_file_name = "datasets/Vendas.csv"
-	output_file_name = "datasets/Vendas_out.csv"
-	hook = HookPandas()
-	ingester = IngesterPandas(hook, input_file_name)
-	persister = PersisterPandas(hook, output_file_name)
+    input_file_name = "datasets/Vendas.csv"
+    output_file_name = "datasets/Vendas_out.csv"
+    hook = HookPandas()
+    ingester = IngesterPandas(hook, input_file_name)
+    persister = PersisterPandas(hook, output_file_name)
+    transformer = TransformerPandas()
 
-	df = ingester.ingest()
-	print(df, df.columns, len(df))
+    df = ingester.ingest()
+    print(df, df.columns, len(df))
 
-	df = df.assign(new_column="new value")
-	print(df, df.columns, len(df))
-	persister.persist(df)
+    df = transformer.set_df(df).transform()
+    print(df, df.columns, len(df))
+    persister.persist(df)
 
 if __name__ == "__main__":
-	main()
+    main()
