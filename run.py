1c5c9b64925802c8d243613dba47edfe45e0bfda
William Kennedy <williamkennedyg7@gmail.com>
2025-12-26 20:54:26 -0300

    Removed sales and client XLSX persister.

--- run.py
+++ run.py
@@ -2,8 +2,6 @@ from etl_simplesvet.pipeline import Pipeline
 from etl_simplesvet.steps.step_ingest_pandas_data_analysis import StepIngestPandasDataAnalysis
 from etl_simplesvet.steps.step_transform_pandas_data_analysis_sales import StepTransformPandasDataAnalysisSales
 from etl_simplesvet.steps.step_transform_pandas_data_analysis_clients import StepTransformPandasDataAnalysisClients
-from etl_simplesvet.steps.step_persister_pandas_data_analysis_sales import StepPersisterPandasDataAnalysisSales
-from etl_simplesvet.steps.step_persister_pandas_data_analysis_clients import StepPersisterPandasDataAnalysisClients
 from etl_simplesvet.steps.step_ingest_pandas_export import StepIngestPandasExport
 from etl_simplesvet.steps.step_transform_pandas_export import StepTransformPandasExport
 from etl_simplesvet.steps.step_persister_pandas_export import StepPersisterPandasExport
@@ -14,8 +12,6 @@ def main():
     ingestion_data_analysis = StepIngestPandasDataAnalysis()
     transform_sales = StepTransformPandasDataAnalysisSales()
     transform_clients = StepTransformPandasDataAnalysisClients()
-    persister_sales = StepPersisterPandasDataAnalysisSales()
-    persister_clients = StepPersisterPandasDataAnalysisClients()
     ingestion_export = StepIngestPandasExport()
     transform_export = StepTransformPandasExport()
     persister_export = StepPersisterPandasExport()
@@ -27,8 +23,6 @@ def main():
     .add_step(ingestion_data_analysis) \
     .add_step(transform_sales) \
     .add_step(transform_clients) \
-    .add_step(persister_sales) \
-    .add_step(persister_clients) \
     .add_step(ingestion_export) \
     .add_step(transform_export) \
     .add_step(persister_export) \
